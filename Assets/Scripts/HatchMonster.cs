32f3ff3d84359787c09321cd370b18b1b2c7cd3a
SocialBlunder <socialblunder@gmail.com>
2015-02-15 05:48:09 -0500

    Added a new HatchMonster enemy with movements and fixed length of Hatch graphic

---
+++ Assets/Scripts/HatchMonster.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HatchMonster : MonoBehaviour {
+
+	private float movementSpeed = 0.2f;
+	private bool moveUp = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (-40.0f > transform.position.y) {
+			moveUp = true;
+		} 
+
+		if (transform.position.y > 14.0f) {
+			moveUp = false;
+		}
+
+		if (moveUp) {
+			transform.position += Vector3.up * movementSpeed;
+		}
+		
+		if (!moveUp) {
+			transform.position += Vector3.down * movementSpeed;
+		}
+	}
+}
