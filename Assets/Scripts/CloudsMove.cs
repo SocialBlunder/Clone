418e54c43c8ebf0af403d5a322d3e935e030852e
litholight <litholight@gmail.com>
2015-02-15 23:07:16 -0500

    Added cloud movement

---
+++ Assets/Scripts/CloudsMove.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudsMove : MonoBehaviour {
+
+	public float speedParam = 2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaDistance = speedParam*Time.deltaTime;
+		this.transform.position = new Vector3 (this.transform.position.x - deltaDistance, this.transform.position.y, this.transform.position.z);
+	}
+}
